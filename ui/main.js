55507b23aefbd9e1c7487fe497069ae6c345c5da
Mistry Anwar Hussain Abdul Gani <anwarhussainmistry@gmail.com>
2017-02-12 19:22:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+var marginRight=0;
+function moveRight(){
+    marginRight=mirginRight+1;
+    movimg.style.marginRight=marginRight+px;
+}
+var element = get.ElementById('movimg');
+element.onclick = function(){
+    var interval=setinterval(moveRight,100);
+};
