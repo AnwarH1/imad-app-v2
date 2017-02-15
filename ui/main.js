ca48451413ed066dc7d6ffaa2c4d5f0faec20e61
Mistry Anwar Hussain Abdul Gani <anwarhussainmistry@gmail.com>
2017-02-15 16:25:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,18 @@ element.onclick = function(){
 var x = document.getElementById("img0");
 x.setAttribute("src", "first-pic.jpg");
 
+
+     var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsById("img1");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
